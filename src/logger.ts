9e02ed52613b538adf3195cc2960c5a655da560d
Jaime Melis <jmelis@redhat.com>
2020-10-29 11:35:18 +0100

    use simple logging instead of JSON
    Since we don't have JSON parse in our kibana, we should use simple logs.
    
    This is an example of this log format:
    
    ```
    $ node ./dist/main-bundle.js
    2020-10-29T10:33:57.591Z INFO: Running at http://localhost:4000/graphql
    2020-10-29T10:34:03.485Z INFO: skipping reload, data already loaded
    2020-10-29T10:34:19.877Z INFO: bundle loaded: 6e84c88f5b4debcf26b393fd3ace05729cf39a8a191e1ba9f30a5c392b906b5a
    2020-10-29T10:34:40.496Z INFO: removing expired bundle: e94972914f41a3ecef5f2b83903c2747da3ee83ba41cc19730c0966b3681f669
    2020-10-29T10:34:40.593Z INFO: bundle loaded: 0954998a34537c38fc6ce9e73f5b1664ab6d10c3e6d70ec0a061b3042ab21743
    ```

--- src/logger.ts
+++ src/logger.ts
@@ -1,8 +1,12 @@
 const { createLogger, format, transports } = require('winston');
-const { combine, timestamp, splat } = format;
+const { combine, timestamp, splat, simple, printf} = format;
+
+const logFormat = printf((info: any) => {
+  return `${info.timestamp} ${info.level.toUpperCase()}: ${ info.message }`;
+});
 
 export const logger = createLogger({
   level: 'info',
-  format: combine(splat(), timestamp(), format.json()),
+  format: combine(timestamp(), splat(), logFormat),
   transports: [new transports.Console()],
 });
