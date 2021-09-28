6c194a04d9d236b56402c88cef9962469de1a54e
Ying Huang <yihuang@redhat.com>
2021-09-28 12:05:26 -0400

    Add yarn install and execution permission to the pr_check.sh

--- pr_check.sh
+++ pr_check.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-yarn run lint
+npm install tslint
+npm install typescript
+npm install mocha
+npm install yarn
 
-yarn test
+CURRENT_DIR=$(pwd)
+
+$CURRENT_DIR/node_modules/yarn/bin/yarn run lint
+
+$CURRENT_DIR/node_modules/yarn/bin/yarn test
