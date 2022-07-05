86d6593d8a5d4a17dbf125d7c1c40633218d0a20
Gerd Oberlechner <goberlec@redhat.com>
2022-07-05 12:41:05 +0200

    resource reference resolver
    resource files are somehow detached from our regular graphql types. they are just references as a path and it is up to the respective integration to fetch the content of such a resource with a dedicated query. this consumes a lot of time during integration runs and pr checks, because such resources are fetched one by one (see terraform-resources defaults or jjb config_path).
    
    `isResource` field definition of type `string` (a.k.a. resource references)  enhanced with `resolveResource: true` will be queryable as graphql type `Resource_v1`
    
    schema.yml
    
    ```yaml
    - name: JenkinsConfig_v1
      fields:
        ...
        - { name: config_path, type: string, isResource: true, resolveResource: true }
    ```
    
    query
    
    ```
    {
      jenkins_configs: jenkins_configs_v1 {
        name
        ...
        type
        config
        config_path {
          content
          sha256sum
        }
      }
    }
    ```
    
    part of https://issues.redhat.com/browse/APPSRE-5950
    schema change: https://github.com/app-sre/qontract-server/pull/147
    
    Signed-off-by: Gerd Oberlechner <goberlec@redhat.com>

---
+++ test/resourceref/resourceref.test.ts
@@ -0,0 +1,46 @@
+import * as http from 'http';
+
+import * as chai from 'chai';
+
+// Chai is bad with types. See:
+// https://github.com/DefinitelyTyped/DefinitelyTyped/issues/19480
+import chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+
+import * as server from '../../src/server';
+import * as db from '../../src/db';
+
+const should = chai.should();
+
+describe('clusters', async() => {
+  let srv: http.Server;
+  before(async() => {
+    process.env.LOAD_METHOD = 'fs';
+    process.env.DATAFILES_FILE = 'test/resourceref/data.json';
+    const app = await server.appFromBundle(db.getInitialBundles());
+    srv = app.listen({ port: 4000 });
+  });
+
+  it('resolve resource refs', async() => {
+    const query = `
+      {
+        test: test_type_v1 {
+          name
+          unresolvable_resource_ref
+          resolvable_resource_ref {
+            content
+          }
+        }
+      }
+      `;
+    const resp = await chai.request(srv)
+                        .post('/graphql')
+                        .set('content-type', 'application/json')
+                        .send({ query });
+    resp.should.have.status(200);
+    resp.body.extensions.schemas.should.eql(['/test-type-1.yml']);
+    resp.body.data.test[0].name.should.equal('name');
+    resp.body.data.test[0].unresolvable_resource_ref.should.equal('/resource1.yml');
+    return resp.body.data.test[0].resolvable_resource_ref.content.should.equal('test resource');
+  });
+});
